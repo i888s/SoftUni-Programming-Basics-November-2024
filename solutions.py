6f6ea15a8c8da00fc92a77f5a840c660d2e1e56e
Ivena Stefanova <ivena.st@abv.bg>
2025-01-13 21:21:22 +0200

    Create solutions.py

---
+++ solutions.py
@@ -0,0 +1,58 @@
+1.1 
+print('Hello SoftUni')
+
+1.2
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+
+1.3
+a=int(input())
+b=int(input())
+area=a*b
+print(area)
+
+1.4
+cm=float(input())
+inch=cm*2.54
+print(inch)
+
+1.5
+name=input()
+print('Hello, ' + name + '!')
+
+1.6
+first_name=input()
+last_name=input()
+age=int(input())
+town=input()
+print(f'You are {first_name} {last_name}, a {age}-years old person from {town}.')
+
+1.7
+architect_name=input()
+projects=int(input())
+hours=projects*3
+print(f"The architect {architect_name} will need {hours} hours to complete {projects} project/s.")
+
+1.8
+dog_food=int(input())
+cat_food=int(input())
+dog_food_price=dog_food*2.50
+cat_food_price=cat_food*4
+total=dog_food_price+cat_food_price
+print(f"{total} lv.")
+
+1.9
+metri=float(input())
+cena=metri*7.61
+discount=cena*0.18
+finalPrice=cena-discount
+print(f"The final price is: {finalPrice} lv.")
+print(f"The discount is: {discount} lv.")
